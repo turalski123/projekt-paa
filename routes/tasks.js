a75aec558d3905db886803a2a0a8bf767446bc3d
Mateusz_Turalski <trzepakdrapak@gmail.com>
2020-04-10 17:51:41 +0000

    Poprawki 00

--- routes/tasks.js
+++ routes/tasks.js
@@ -7,5 +7,9 @@ router.post('/add', async (ctx, next) => {
   await store.createTask(ctx.request.body.title)
   ctx.redirect('/')
 })
-
+router.post('/updateStatus', async (ctx, next) => {
+  const { id, status } = ctx.request.body
+  await store.updateTaskStatus(id, status)
+  ctx.status = 200
+})
 module.exports = router
