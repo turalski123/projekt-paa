dcbab77138059d9dd3529e246903c42848440421
Mateusz_Turalski <trzepakdrapak@gmail.com>
2020-04-10 16:31:15 +0000

    Dodano możliwość zmiany statusu zadania

--- store.js
+++ store.js
@@ -16,7 +16,8 @@ const createTask = async (title) => (
     const task = {
       PartitionKey: generator.String('task'),
       RowKey: generator.String(uuid.v4()),
-      title
+      title,
+      status: 'open'
     }
 
     service.insertEntity(table, task, (error, result, response) => {
@@ -27,19 +28,36 @@ const createTask = async (title) => (
 const listTasks = async () => (
   new Promise((resolve, reject) => {
     const query = new storage.TableQuery()
-      .select(['title'])
+      .select(['RowKey', 'title', 'status'])
       .where('PartitionKey eq ?', 'task')
 
     service.queryEntities(table, query, null, (error, result, response) => {
       !error ? resolve(result.entries.map((entry) => ({
-        title: entry.title._
+        id: entry.RowKey._,
+        title: entry.title._,
+        status: entry.status._
       }))) : reject()
+    })
+  })
+)
+const updateTaskStatus = async (id, status) => (
+  new Promise((resolve, reject) => {
+    const generator = storage.TableUtilities.entityGenerator
+    const task = {
+      PartitionKey: generator.String('task'),
+      RowKey: generator.String(id),
+      status
+    }
+
+    service.mergeEntity(table, task, (error, result, response) => {
+      !error ? resolve() : reject()
     })
   })
 )
 module.exports = {
   init,
   createTask,
-  listTasks
+  listTasks,
+updateTaskStatus
 }
 
